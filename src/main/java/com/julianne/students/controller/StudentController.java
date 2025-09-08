686a4e4a7640d7ba064803a092778ba87293d8f7
juli-anne <julianne.lazar@gmail.com>
2025-09-08 11:32:29 +0200

    add student and update student by id

--- src/main/java/com/julianne/students/controller/StudentController.java
+++ src/main/java/com/julianne/students/controller/StudentController.java
@@ -3,9 +3,9 @@ package com.julianne.students.controller;
 import com.julianne.students.entity.Student;
 import com.julianne.students.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
@@ -14,17 +14,41 @@ import java.util.List;
 public class StudentController {
 
     @Autowired
-    StudentRepository StudentRepository;
+    StudentRepository studentRepository;
     // get all the students
     @GetMapping("/students")
     public List<Student> getAllStudents() {
-        List<Student> students = StudentRepository.findAll();
+        List<Student> students = studentRepository.findAll();
         return students;
     }
 
+    // get student by id
     @GetMapping("/student/{id}")
     public List<Student> getStudent(@PathVariable Integer id) {
-        Student student = StudentRepository.findById(id).get();
+        Student student = studentRepository.findById(id).get();
         return Collections.singletonList(student);
+    }
+
+    // add new student
+    @PostMapping("/student/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createStudent(@RequestBody Student student) {
+        studentRepository.save(student);
+    }
+
+    // update a student
+    @PutMapping("/student/update/{id}")
+    public ResponseEntity<Student> updateStudent(
+            @PathVariable Integer id,
+            @RequestBody Student updatedStudent) {
+        return studentRepository.findById(id)
+                .map(student -> {
+                    student.setFirstName(updatedStudent.getFirstName());
+                    student.setLastName(updatedStudent.getLastName());
+                    student.setProgram(updatedStudent.getProgram());
+                    Student saved = studentRepository.save(student);
+                    return ResponseEntity.ok(saved);
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 }
--- src/main/java/com/julianne/students/repository/StudentRepository.java
+++ src/main/java/com/julianne/students/repository/StudentRepository.java
@@ -4,4 +4,5 @@ import com.julianne.students.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+    Integer id(Long id);
 }
